a6d2368eeed915abf35fac81020d89e8342d4e47
Maxence <max.verdom@gmail.com>
2024-01-31 09:13:00 +0100

    remove useless files and retry pipeline

--- server/jest-mongodb-config.js
+++ server/jest-mongodb-config.js
@@ -3,10 +3,10 @@ module.exports = {
     instance: {
       port: 27018, // Utilisez un port différent du port par défaut
     },
-    // binary: {
-    //   version: "4.0.3", // Utilisez la version de MongoDB que vous préférez
-    //   skipMD5: true,
-    // },
+    binary: {
+      version: "4.0.3", // Utilisez la version de MongoDB que vous préférez
+      skipMD5: true,
+    },
     autoStart: false,
   },
 };
