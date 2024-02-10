ff1e8c87221ab1d8941074acb1dff6cc5c741a24
Maxence <max.verdom@gmail.com>
2024-02-10 15:08:19 +0100

    .gitignore is now working

--- client/jest.setup.js
+++
@@ -1,7 +0,0 @@
-if (window.HTMLFormElement) {
-  window.HTMLFormElement.prototype.requestSubmit = function () {
-    if (this.dispatchEvent(new Event("submit"))) {
-      this.submit();
-    }
-  };
-}
--- client/src/components/contact/ContactApp.js
+++
@@ -1,6 +0,0 @@
-const contactForm = document.querySelector(".form-contact");
-
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("submit clicked");
-});
