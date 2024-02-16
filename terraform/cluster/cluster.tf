c1212602ab6aea038a89d1f9b484c572c17f61a8
Maxence <max.verdom@gmail.com>
2024-02-16 15:08:36 +0100

    try add pool to deploy ECK, add autoscaling

--- terraform/cluster/cluster.tf
+++ terraform/cluster/cluster.tf
@@ -10,7 +10,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = var.pool_name
   location   = var.gcp_zone
   cluster    = google_container_cluster.primary.name
-  node_count = 2
 
   node_config {
     preemptible  = true
@@ -31,7 +30,6 @@ resource "google_container_node_pool" "secondary_preemptible_nodes" {
   name       = var.pool_name_eck
   location   = var.gcp_zone
   cluster    = google_container_cluster.primary.name
-  node_count = 2
 
   node_config {
     preemptible  = true
