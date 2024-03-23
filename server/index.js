b1734aff2e0be9f4e55f2163b26dcd962bd60f7e
Maxence <max.verdom@gmail.com>
2024-03-23 13:03:42 +0100

    try remove middleware

--- server/index.js
+++ server/index.js
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 app.use(express.static("client/build"));
 app.use(cors());
 
-app.use('/api', createProxyMiddleware({ 
-  target: 'http://back-service.default.svc.cluster.local:7000', 
-  changeOrigin: true,
-}));
+// app.use('/api', createProxyMiddleware({ 
+//   target: 'http://back-service.default.svc.cluster.local:7000', 
+//   changeOrigin: true,
+// }));
 
 app.use("/api/tech", techRoutes);
 app.use("/api/images", imagesRoutes);
